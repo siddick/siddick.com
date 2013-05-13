856ba77e1007b3ee1f556edc4c56f607c99177bb
siddick <siddick@gmail.com>
2013-05-13 23:57:17 +0530

    added links for tag

--- lib/link_helper.rb
+++ lib/link_helper.rb
@@ -1,4 +1,7 @@
 module LinkHelper
+  include Nanoc::Helpers::LinkTo
+  include Nanoc::Toolbox::Helpers::HtmlTag
+
   def relative_asset_path(path)
     relative_path_to(asset_path(path))
   end
@@ -13,6 +16,16 @@ module LinkHelper
       link_to(name, relative_path_to(path)),
       { :class => class_name }.merge(options))
   end
+
+  def tag_links(tags)
+    if tags
+      content_tag(:span, "", :class => "icon-tag") + " " +
+        tags.split(Seperator).map do |tag|
+          link_to(tag, relative_path_to("/tags/#{tag}/"))
+        end.join(", ")
+    end
+  end
+
 end
 
 include LinkHelper
