d8314dbdf8592b466ac0be9ab126501fb1c91f8d
siddick <siddick@gmail.com>
2017-02-26 07:46:43 +0530

    Update layout

--- lib/item_helper.rb
+++ lib/item_helper.rb
@@ -12,7 +12,7 @@ module ItemHelper
   end
 
   def create_article_items(items, identifier, options = {})
-    list = paginate(items)
+    list = paginate(items, options)
     path = identifier
     list.each_with_index do |items, page|
       page += 1
@@ -24,6 +24,11 @@ module ItemHelper
     end
   end
 
+  def article_data(date)
+      date = Time.parse(date)
+      Time.now.year == date.year ? date.strftime("%d %b") : date.strftime("%d %b %Y")
+  end
+
   def page_identifier(identifier, page)
     if page == 1
       identifier
@@ -32,9 +37,10 @@ module ItemHelper
     end
   end
 
-  def paginate(items)
+  def paginate(items, options)
     items = items.select{|item| item[:created_at] }.
       sort{|a, b| Time.parse(b[:created_at]) <=> Time.parse(a[:created_at]) }
+    items.reverse! if (options[:tag])
     list  = []
     until items.empty?
       list << items.slice!(0..(PageLimit - 1))
